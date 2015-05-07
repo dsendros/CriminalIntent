947b6d85f306fbe5b2bcdc0b2043896ae001c527
dsendros <dsendros@gmail.com>
2015-05-06 22:29:25 -0400

    Initialized the listview

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -17,6 +18,10 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crimes_title);
 
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                android.R.layout.simple_list_item_1, mCrimes);
+        setListAdapter(adapter);
     }
 
 }
