aff25ed4b4ec6a8c84b5dfcecbefab17bd07b827
dsendros <dsendros@gmail.com>
2015-05-19 22:53:00 -0400

    Chapter 11 - Using Viewpager

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,8 +1,15 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,6 +18,7 @@ import java.util.ArrayList;
  */
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
+    private static final String TAG = "CrimeListFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,9 +27,55 @@ public class CrimeListFragment extends ListFragment {
 
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
 
-        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
-                android.R.layout.simple_list_item_1, mCrimes);
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id)  {
+        //Get the crime from the adapter
+        Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
+
+        //Start CrimePagerActivity
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+        startActivity(i);
+    }
+
+    @Override
+    public void onResume()  {
+        super.onResume();
+        ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime>  {
+        public CrimeAdapter(ArrayList<Crime> crimes)    {
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)   {
+            //If we aren't given a view, inflate one
+            if (convertView == null)    {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_crime, null);
+            }
+
+            //Configure the view for this Crime
+            Crime c = getItem(position);
+
+            TextView titleTextView =
+                    (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView =
+                    (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox =
+                    (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
+        }
     }
 
 }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,65 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Dennis on 5/19/2015.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewpager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public int getCount()   {
+                return mCrimes.size();
+            }
+
+            @Override
+            public Fragment getItem(int pos) {
+                Crime crime = mCrimes.get(pos);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+        });
+
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for (int i = 0; i < mCrimes.size(); i++)    {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            public void onPageScrollStateChanged(int state)    {}
+
+            public void onPageScrolled(int pos, float posOffset, int posOffsetPixels)  {}
+            
+            public void onPageSelected(int pos)    {
+                Crime crime = mCrimes.get(pos);
+                if (crime.getTitle() != null)   {
+                    setTitle(crime.getTitle());
+                }
+            }
+        });
+    }
+}
